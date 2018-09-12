88fa9daa748d7d44cf200266ffc538ee1075e13e
Arael Sidh <omegaari@hotmail.com>
2018-09-11 22:03:50 -0300

    Mockup start state, music, player starting position fix

--- source/Main.hx
+++ source/Main.hx
@@ -4,13 +4,14 @@ import flixel.FlxGame;
 import openfl.display.Sprite;
 import states.EndState;
 import states.PlayState;
+import states.StartState;
 
 class Main extends Sprite {
 	
 	public function new() {
 		super();
 		
-		addChild(new FlxGame(256, 240, EndState));
+		addChild(new FlxGame(256, 240, StartState));
 	}
 	
 }
